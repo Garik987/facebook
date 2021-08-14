a355a63dcd0459cf2707e901638cd4217fafe5f6
Garik987 <tovgar01@mail.ru>
2021-08-14 19:49:35 +0400

    dfdfdfD

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    class  A{
+        final function  velcom(){
+            #final methody  jarang clasnerum chkrknelu hamr a
+        }
+    }
+#SELF Y OGTAGORCVUM A VOH MIAYN CONST ERI JAMANAK AYL STATIC FUNCERI HAMAR
+    class B{
+        public static $x=10;
+        public  function  getX(){
+            return self::$x;
+        }
+    }
+    echo  B::$x."<br>";
+    abstract class AB{
+        public  function  getUser(){
+            return "John Smith";
+        }
+    }
+    #abstractctic obj chi linum stexcel
+    #drahamar nranic jarangum enq nor clas ,dranic heto jarangic stexcum enq obj
+    #abstracty partadrum a irenic cnvac classnerin  unena ir bolor hatkutyunnery,isk sovorakani depqum che
+    #abstract functiony kara nkaragrutyun({}) chunena u ira jarangi mech grvi nkaragrutyny
+class  BB extends  AB{
+
+}
+$x=new BB();
+    echo $x->getUser();
+#gi da mi proekti var mi qani hogi ashxatelu hamar  mijavayr a
+# git status ete karmir a mer papken uremy git in kpac chi
+#fayleri het ashxatelu makardakner
+# 1in mac untracked faylery miacac chen git um
+# 2rd mac commit avelacac en baych amen popoxutyan hamar coment petq a anenq
+# 3rd tracked miacac a
+# 4rd push arden uxarkum a
+# git add filer avelacnel (. erb bolory miangamic a qcum
+# git commit -m "msg" commit makardak ancnel hamar)
+#
